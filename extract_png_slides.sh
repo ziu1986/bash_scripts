30c902839e2eee43f34311b87015a68d952932e8
Stefanie Falk <stefanie.falk@geo.uio.no>
2021-09-21 09:13:52 +0200

    Generalization

--- extract_png_slides.sh
+++ extract_png_slides.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
-pdfseparate slides_EGU21-15000_sf.pdf video/slides_EGU21-15000_sf_%d.pdf
-cd video
+INPUT=${1}
+OUTPUT_DIR=${2}
+
+if [ ! -d $OUTPUT_DIR ]; then
+    mkdir -p $OUTPUT_DIR
+fi
+
+SEPERATOR="`basename ${INPUT} .pdf`_%d.pdf"
+
+pdfseparate $INPUT $OUTPUT_DIR/$SEPERATOR
+
+cd $OUTPUT_DIR
+
 for each in `ls *.pdf`; do
-    png_file=`basename -s .pdf ${each}`.png
+    png_file="`basename ${each} .pdf`.png"
     echo $each $png_file
     pdftoppm -png $each > $png_file
 done
